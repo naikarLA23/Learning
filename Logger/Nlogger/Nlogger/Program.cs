cf373f64c6a538cf62af6261b68242267727b7f9
L A Naikar <70810522+naikarLA23@users.noreply.github.com>
2025-01-11 21:26:50 +0530

    Add files via upload

---
+++ Logger/Nlogger/Nlogger/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace Nlogger
+{
+    internal class Program
+    {
+
+        static void Main(string[] args)
+        {
+            Logger.LogFatal("Logging FATAL message");
+            Logger.LogError("Logging ERROR message");
+            Logger.LogWarning("Logging WARNING message");
+            Logger.LogDebug("Logging DEBUG message");
+            Logger.LogInfo("Logging INFO message");
+            Logger.LogTrace("Logging TRACE message");
+
+            Console.ReadLine();
+        }
+    }
+}
