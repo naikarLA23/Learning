b0c0ab22d8ae1a27bf3df3c7c2baedaf1ecd4e52
L A Naikar <70810522+naikarLA23@users.noreply.github.com>
2025-01-12 00:12:53 +0530

    Add files via upload

---
+++ Logger/Nlogger/Nlogger/Logger.cs
@@ -0,0 +1,90 @@
+﻿using NLog;
+using NLog.Config;
+using NLog.Layouts;
+using NLog.Targets;
+using NLog.Targets.Wrappers;
+using System.Reflection;
+
+namespace Nlogger
+{
+    internal static class Logger
+    {
+
+        public static NLog.Logger NloggerObject { get; private set; }
+
+        static Logger()
+        {
+           NloggerObject = LogManager.GetCurrentClassLogger();
+        }
+
+        internal static void SetChangeFileProperties()
+        {
+            var fileTarget = NloggerObject.Factory.Configuration.AllTargets.First(a => a.Name == "file") as FileTarget;
+            fileTarget.FileName = Path.Combine(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location), "Logs", 
+                DateTime.Now.ToString("ddMMyyyy"), "NewApplication.log");  
+
+            var config = new LoggingConfiguration();
+            var asyncFileTarget = new AsyncTargetWrapper(fileTarget);
+
+            config.AddTarget("file", asyncFileTarget);
+        }
+
+        internal static void RemoveTarget(string name)
+        {
+            LogManager.Configuration.RemoveTarget(name);
+        }
+
+        internal static void AddNewFileTarget(string logPath, string logFileName)
+        {
+            var fileTarget = new FileTarget
+            {
+                CreateDirs = true,
+                Layout = Layout.FromString("${date:format=yyyy-MM-dd HH\\:mm\\:ss.fffffff} ${level} ${message}"),
+                FileName = String.Format("{0}\\{1}", logPath, logFileName),
+                ArchiveFileName = Path.Combine(logPath, logFileName, "{#####}"),
+                ArchiveNumbering = ArchiveNumberingMode.Sequence,
+                ArchiveEvery = FileArchivePeriod.None,
+                ArchiveAboveSize = 10 * 1024 * 1024,
+                AutoFlush = true,
+                ConcurrentWrites = false,
+                BufferSize = 10240,
+                Name="file"
+            };
+           
+            LogManager.Configuration.AddTarget(fileTarget);
+            var fileLoggingRule = new LoggingRule("*", LogLevel.Trace, fileTarget);
+            LogManager.Configuration.LoggingRules.Add(fileLoggingRule);
+            LogManager.ReconfigExistingLoggers();
+        }
+
+        internal static void LogFatal(string message)
+        {
+            NloggerObject.Fatal(message);
+        }
+
+        internal static void LogError(string message)
+        {
+            NloggerObject.Error(message);
+        }
+
+        internal static void LogWarning(string message)
+        {
+            NloggerObject.Warn(message);
+        }
+
+        internal static void LogDebug(string message)
+        {
+            NloggerObject.Debug(message);
+        }
+
+        internal static void LogInfo(string message)
+        {
+            NloggerObject.Info(message);
+        }
+
+        internal static void LogTrace(string message)
+        {
+            NloggerObject.Trace(message);
+        }
+    }
+}
---
+++ Logger/Nlogger/Nlogger/Program.cs
@@ -0,0 +1,29 @@
+﻿using System.Reflection;
+
+namespace Nlogger
+{
+    internal class Program
+    {
+
+        static void Main(string[] args)
+        {
+            Logger.LogFatal("Logging FATAL message");
+            Logger.LogError("Logging ERROR message");
+            Logger.LogWarning("Logging WARNING message");
+            Logger.LogDebug("Logging DEBUG message");
+            Logger.LogInfo("Logging INFO message");
+            Logger.LogTrace("Logging TRACE message");
+
+            Logger.SetChangeFileProperties();
+            Logger.LogDebug("Logging new 1 DEBUG message");
+
+            Logger.RemoveTarget("file");
+
+            var filePath = Path.Combine(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location), "Logs", DateTime.Now.ToString("ddMMyyyy"));
+            Logger.AddNewFileTarget(filePath, "NewApplication2.log");
+            Logger.LogDebug("Logging new 2 DEBUG message");
+
+            Console.ReadLine();
+        }
+    }
+}
